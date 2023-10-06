bab47fe7fcfc9741cc9e67e2f008109b8727e3ae
BoTSaiiF <131559118+BoTSaiiF@users.noreply.github.com>
2023-10-06 23:14:56 +0300

    k.py

---
+++ k.py
@@ -0,0 +1,4 @@
+import requests,time
+for i in range(1000000000000):
+	time.sleep(30)
+	requests.post(f"https://api.telegram.org/bot5987480181:AAE8ZZAA6aduNEdnuoI3mA9kT-wDZEkNiNY/sendMessage?chat_id=-1001795855929&text=تيست")
